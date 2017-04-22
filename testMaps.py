f8aed30fa41b571fdd3a50bcd5f4ed9e5d24ae76
VivekKumar <vivekkumar@localhost.localdomain>
2017-04-21 23:22:57 -0400

    Testing Google Maps API

--- testMaps.py
+++ testMaps.py
@@ -1,18 +1,26 @@
-'''
-Created on Apr 21, 2017
-
-@author: roop
-'''
 import googlemaps
 from datetime import datetime
+import json
+gmaps = googlemaps.Client(key='AIzaSyDSPgOMDFfBFW3YvwHHpNdvBSy838l3NBI')
 
-gmaps = googlemaps.Client(key="AIzaSyDSPgOMDFfBFW3YvwHHpNdvBSy838l3NBI")
-geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+# Geocoding an address
+#geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+origin = "3892 Alder Avenue, Freemont, CA"
+destination = "454 Tennessee Ln, Palo Alto, CA"
+
+travel_time = gmaps.directions(origin,destination)
+
+print travel_time
+
+'''
+# Look up an address with reverse geocoding
 reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
 now = datetime.now()
 directions_result = gmaps.directions("Sydney Town Hall",
                                      "Parramatta, NSW",
                                      mode="transit",
                                      departure_time=now)
-
-print directions_rest
+'''
